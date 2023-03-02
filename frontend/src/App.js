4282353eae29b40e26552f5f1d46f1bc14e416c7
Marvellous Adesanya <marvellousniyi123@gmail.com>
2023-03-02 12:53:21 +0000

    Installed React and dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="text-3xl font-bold text-red-500">Testing Tailwind</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
