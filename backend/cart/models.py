4e0894d2e740a575b8136274bae6e79f9027a171
Beloved-vince <vinceoludare@gmail.com>
2023-03-16 11:38:12 +0100

    update models cartitem

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -4,7 +4,6 @@ from store.models import Product
 
 
 # Create your models here.
-
 class Cart(models.Model):
     cart_id = models.CharField(max_length=200, blank=True)
     date_added = models.DateField(auto_now_add=True)
@@ -18,7 +17,7 @@ class CartItem(models.Model):
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
     
-    def __str__(self):
+    def __repr__(self):
         return self.product
     #class Meta:
     #    db_table='product_description'
