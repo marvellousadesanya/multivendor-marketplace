6d22ef7465151e1ae064f7a76cee263c32ab43f8
Marv Adesanya <marvellousniyi123@gmail.com>
2023-09-10 00:03:20 +0100

    Dynamic routing

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "4500",
+        pathname: "/product-image/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
